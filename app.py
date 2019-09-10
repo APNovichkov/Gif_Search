708f5cf15130e6d4f01d1a8e1e7c6682d977dcf1
Andrey Novichkov <andrey.novichkov@gmail.com>
2019-09-10 11:58:07 -0700

    [addition][improvement] Added main.css file and wrote a working API implementation of the Tenor gid search

--- app.py
+++ app.py
@@ -10,17 +10,29 @@ def index():
     """Return homepage."""
     # TODO: Extract query term from url
     search_input = request.args.get('search_input')
-    print("search_input: " + str(search_input))
+
     # TODO: Make 'params' dict with query term and API key
-
+    params = {
+        "api_key": "XE9GOKSZYLQB",
+        "query_term": search_input,
+        "num_gifs_to_load": 8
+    }
     # TODO: Make an API call to Tenor using the 'requests' library
+    tenor_request = requests.get("https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (params.get("query_term"),
+                                 params.get("api_key"), params.get("num_gifs_to_load")))
 
+    loaded_gifs = None
     # TODO: Get the first 10 results from the search results
+    if tenor_request.status_code == 200:
+        loaded_gifs = json.loads(tenor_request.content)
+    else:
+        loaded_gifs = none
 
     # TODO: Render the 'index.html' template, passing the gifs as a named parameter
 
     return render_template(
         "index.html",
+        loaded_gifs=loaded_gifs,
         search_input=search_input)
 
 
