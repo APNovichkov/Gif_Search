607497c123a66363da470b6722481b0ecfbfc73a
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2019-09-19 16:04:18 -0700

    [fix] buttons to be inline

--- app.py
+++ app.py
@@ -11,10 +11,12 @@ def index():
     # TODO: Extract query term from url
     search_input = request.args.get('search_input')
     top_10 = request.args.get('top_10')
+    random = request.args.get('random')
 
     # set the apikey and limit
     apikey = "LIVDSRZULELA"
     lmt = 9
+
     # TODO: Make 'params' dict with query term and API key
     params = {
         "api_key": "XE9GOKSZYLQB",
@@ -24,20 +26,26 @@ def index():
     # TODO: Make an API call to Tenor using the 'requests' library
     tenor_request = requests.get("https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (params.get("query_term"),
                                  params.get("api_key"), params.get("num_gifs_to_load")))
-    # get the top 10 trending GIFs - using the default locale of en_US
+    # get the top trending GIFs - using the default locale of en_US
     top_10_gifs = requests.get("https://api.tenor.com/v1/trending?key=%s&limit=%s" % (apikey, lmt))
 
-    loaded_gifs = None
+    # get the top random GIFs - using the default locale of US 
+    random = requests.get("https://api.tenor.com/v1/random?key=%s&limit=%s" % (apikey, lmt))
+
     # TODO: Get the first 10 results from the search results
+    loaded_gifs = None 
 
     print("Tenor status code: " + str(tenor_request.status_code))
     print("Top_10 value: " + str(top_10))
 
     if top_10 == "true" or tenor_request.status_code != 200 or len(tenor_request.content) == 0:
         loaded_gifs = json.loads(top_10_gifs.content)
+    elif random == "true":
+        loaded_gifs = json.loads(random.content)
     else:
         loaded_gifs = json.loads(tenor_request.content)
 
+    
     # TODO: Render the 'index.html' template, passing the gifs as a named parameter
     return render_template(
         "index.html",
